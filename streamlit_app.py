7b00398d8ef86bd52fbb8e035518f3b12034b3c4
kallamrameshbabu <104750577+kallamrameshbabu@users.noreply.github.com>
2023-10-19 20:10:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,19 +38,12 @@ try:
 
 except URLError as e:
   streamlit.error()
-
-streamlit.header("The fruit load list contains:")
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-        return my_cur.fetchall()
-
+        
 # Allow end users to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute ("insert into fruit_load_list values ('from streamlit')")
+        my_cur.execute ("insert into fruit_load_list values ('"+ new_fruit +"')")
         return "Thanks for adding" + new_fruit
-
 
 add_my_fruit = streamlit.text_input('VIEW OUR FRUIT LIST - ADD YOUR FAVORITES!')
 if streamlit.button('Get Fruit List'):
